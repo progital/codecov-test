6074eb778a255d86b002fc2c238eb5c270850aec
andrey <dev@progital.io>
2021-12-06 15:11:15 +0200

    more helper functions

--- src/helpers.js
+++ src/helpers.js
@@ -6,4 +6,12 @@ function sub(num1, num2) {
   return num1 - num2;
 }
 
-module.exports = { sum, sub };
+function div(num1, num2) {
+  return num1 / num2;
+}
+
+function pow(num1, num2) {
+  return num1 ** num2;
+}
+
+module.exports = { sum, sub, div, pow };
