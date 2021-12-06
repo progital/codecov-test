f0cf5623034638e918fdc06fbff37511aa0942f6
andrey <dev@progital.io>
2021-12-06 15:29:08 +0200

    added mult fn

--- src/helpers.js
+++ src/helpers.js
@@ -10,8 +10,12 @@ function div(num1, num2) {
   return num1 / num2;
 }
 
+function mult(num1, num2) {
+  return num1 * num2;
+}
+
 function pow(num1, num2) {
   return num1 ** num2;
 }
 
-module.exports = { sum, sub, div, pow };
+module.exports = { sum, sub, div, pow, mult };
